ee7ed0d2bb7af6ec72a8463706eda28486d24347
pixelite01 <108801292+pixelite01@users.noreply.github.com>
2022-09-29 10:50:59 +0700

    Update run2.py

--- run2.py
+++ run2.py
@@ -43,19 +43,19 @@ for x in range(1000):
 	ugen2.append(uaku)
 	
 	aa='Mozilla/5.0 (Linux; U; Android'
-    b=random.choice(['8','9','10','11','12'])
-    c='en-US; V2141 Build/SP1A.210812.003'
-    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-    e=random.randrange(1, 999)
-    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-    g='AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
-    h=random.randrange(73,200)
-    i='0'
-    j=random.randrange(4500,5900)
-    k=random.randrange(40,200)
-    l='UCBrowser/13.4.0.1306 Mobile Safari/537.36'
-    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
-    ugen.append(uaku2)
+    	b=random.choice(['8','9','10','11','12'])
+   	c='en-US; V2141 Build/SP1A.210812.003'
+    	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    	e=random.randrange(1, 999)
+    	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    	g='AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+    	h=random.randrange(73,200)
+    	i='0'
+   	j=random.randrange(4500,5900)
+    	k=random.randrange(40,200)
+    	l='UCBrowser/13.4.0.1306 Mobile Safari/537.36'
+    	uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    	ugen.append(uaku2)
 for x in range(10):
 	a='Mozilla/5.0 (SAMSUNG; SAMSUNG-GT-S'
 	b=random.randrange(100, 9999)
