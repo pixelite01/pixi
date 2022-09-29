ef088120ba9a67598107b6efa8089ffe8e723644
pixelite01 <108801292+pixelite01@users.noreply.github.com>
2022-09-29 10:52:17 +0700

    Update run2.py

--- run2.py
+++ run2.py
@@ -40,22 +40,7 @@ for x in range(1000):
 	C = f'(KHTML, like Gecko) UCBrowser/79.0.{str(rr(1111,9999))}.136 Mobile Safari'
 	D = f'/537.36 Puffin/9.7.2.{str(rr(11111,99999))}AP'
 	uaku = f'{A}{B}{C}{D}'
-	ugen2.append(uaku)
-	
-	aa='Mozilla/5.0 (Linux; U; Android'
-    	b=random.choice(['8','9','10','11','12'])
-   	c='en-US; V2141 Build/SP1A.210812.003'
-    	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-    	e=random.randrange(1, 999)
-    	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-    	g='AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
-    	h=random.randrange(73,200)
-    	i='0'
-   	j=random.randrange(4500,5900)
-    	k=random.randrange(40,200)
-    	l='UCBrowser/13.4.0.1306 Mobile Safari/537.36'
-    	uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
-    	ugen.append(uaku2)
+	ugen.append(uaku)
 for x in range(10):
 	a='Mozilla/5.0 (SAMSUNG; SAMSUNG-GT-S'
 	b=random.randrange(100, 9999)
